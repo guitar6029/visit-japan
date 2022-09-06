b599803b0f2cdef10dceb1afb51dc63929b913ce
Josh <jsbusiness305@gmail.com>
2022-09-06 15:35:21 -0400

    added tour details for the tours page

--- pages/tours.js
+++ pages/tours.js
@@ -1,12 +1,212 @@
-import Nav from "../components/Nav/Nav"
+import Nav from "../components/Nav/Nav";
+import style from "../styles/Tours.module.css";
 
 function Tours() {
   return (
     <section className="flex_column margin_10">
-      <Nav/>
+      <Nav />
       <h3 className="section_title">TOP TOURS</h3>
+      <div className={style.tours}>
+        <div className={style.tour_card}>
+          <div className={style.tour_title}>
+            <div className={style.title}>
+              <h3>Discover Japan</h3>
+            </div>
+            <div className={style.tour_location}>
+              <h3>Tokyo, Hakone, Kyoto, Osaka</h3>
+            </div>
+          </div>
+          <div className={style.tour_description}>
+            <ul>
+              <li>
+                Explore Tokyo from ancient shrines to ultramodern architecture{" "}
+              </li>
+              <li>
+                Take a dip in a private hot spring in the foothills of Mt. Fuji
+              </li>
+              <li>
+                Enjoy a formal welcome to the country through a private tea
+                ceremony{" "}
+              </li>
+              <li>Get lost among Arashiyama's towering bamboo groves</li>
+              <li>Stay at a handpicked selection of premium accommodations.</li>
+            </ul>
+          </div>
+          <div className={style.price_description}>
+            <h3>Starting from $4712</h3>
+          </div>
+          <div className={style.more_info}>
+            <a href="#">More Info</a>
+          </div>
+        </div>
+
+        <div className={style.tour_card}>
+          <div className={style.tour_title}>
+            <div className={style.title}>
+              <h3>Discover Japan</h3>
+            </div>
+            <div className={style.tour_location}>
+              <h3>Tokyo, Hakone, Kyoto, Osaka</h3>
+            </div>
+          </div>
+          <div className={style.tour_description}>
+            <ul>
+              <li>
+                Explore Tokyo from ancient shrines to ultramodern architecture{" "}
+              </li>
+              <li>
+                Take a dip in a private hot spring in the foothills of Mt. Fuji
+              </li>
+              <li>
+                Enjoy a formal welcome to the country through a private tea
+                ceremony{" "}
+              </li>
+              <li>Get lost among Arashiyama's towering bamboo groves</li>
+              <li>Stay at a handpicked selection of premium accommodations.</li>
+            </ul>
+          </div>
+          <div className={style.price_description}>
+            <h3>Starting from $4712</h3>
+          </div>
+          <div className={style.more_info}>
+            <a href="#">More Info</a>
+          </div>
+        </div>
+
+        <div className={style.tour_card}>
+          <div className={style.tour_title}>
+            <div className={style.title}>
+              <h3>Discover Japan</h3>
+            </div>
+            <div className={style.tour_location}>
+              <h3>Tokyo, Hakone, Kyoto, Osaka</h3>
+            </div>
+          </div>
+          <div className={style.tour_description}>
+            <ul>
+              <li>
+                Explore Tokyo from ancient shrines to ultramodern architecture{" "}
+              </li>
+              <li>
+                Take a dip in a private hot spring in the foothills of Mt. Fuji
+              </li>
+              <li>
+                Enjoy a formal welcome to the country through a private tea
+                ceremony{" "}
+              </li>
+              <li>Get lost among Arashiyama's towering bamboo groves</li>
+              <li>Stay at a handpicked selection of premium accommodations.</li>
+            </ul>
+          </div>
+          <div className={style.price_description}>
+            <h3>Starting from $4712</h3>
+          </div>
+          <div className={style.more_info}>
+            <a href="#">More Info</a>
+          </div>
+        </div>
+
+        <div className={style.tour_card}>
+          <div className={style.tour_title}>
+            <div className={style.title}>
+              <h3>Discover Japan</h3>
+            </div>
+            <div className={style.tour_location}>
+              <h3>Tokyo, Hakone, Kyoto, Osaka</h3>
+            </div>
+          </div>
+          <div className={style.tour_description}>
+            <ul>
+              <li>
+                Explore Tokyo from ancient shrines to ultramodern architecture{" "}
+              </li>
+              <li>
+                Take a dip in a private hot spring in the foothills of Mt. Fuji
+              </li>
+              <li>
+                Enjoy a formal welcome to the country through a private tea
+                ceremony{" "}
+              </li>
+              <li>Get lost among Arashiyama's towering bamboo groves</li>
+              <li>Stay at a handpicked selection of premium accommodations.</li>
+            </ul>
+          </div>
+          <div className={style.price_description}>
+            <h3>Starting from $4712</h3>
+          </div>
+          <div className={style.more_info}>
+            <a href="#">More Info</a>
+          </div>
+        </div>
+
+        <div className={style.tour_card}>
+          <div className={style.tour_title}>
+            <div className={style.title}>
+              <h3>Discover Japan</h3>
+            </div>
+            <div className={style.tour_location}>
+              <h3>Tokyo, Hakone, Kyoto, Osaka</h3>
+            </div>
+          </div>
+          <div className={style.tour_description}>
+            <ul>
+              <li>
+                Explore Tokyo from ancient shrines to ultramodern architecture{" "}
+              </li>
+              <li>
+                Take a dip in a private hot spring in the foothills of Mt. Fuji
+              </li>
+              <li>
+                Enjoy a formal welcome to the country through a private tea
+                ceremony{" "}
+              </li>
+              <li>Get lost among Arashiyama's towering bamboo groves</li>
+              <li>Stay at a handpicked selection of premium accommodations.</li>
+            </ul>
+          </div>
+          <div className={style.price_description}>
+            <h3>Starting from $4712</h3>
+          </div>
+          <div className={style.more_info}>
+            <a href="#">More Info</a>
+          </div>
+        </div>
+
+        <div className={style.tour_card}>
+          <div className={style.tour_title}>
+            <div className={style.title}>
+              <h3>Discover Japan</h3>
+            </div>
+            <div className={style.tour_location}>
+              <h3>Tokyo, Hakone, Kyoto, Osaka</h3>
+            </div>
+          </div>
+          <div className={style.tour_description}>
+            <ul>
+              <li>
+                Explore Tokyo from ancient shrines to ultramodern architecture{" "}
+              </li>
+              <li>
+                Take a dip in a private hot spring in the foothills of Mt. Fuji
+              </li>
+              <li>
+                Enjoy a formal welcome to the country through a private tea
+                ceremony{" "}
+              </li>
+              <li>Get lost among Arashiyama's towering bamboo groves</li>
+              <li>Stay at a handpicked selection of premium accommodations.</li>
+            </ul>
+          </div>
+          <div className={style.price_description}>
+            <h3>Starting from $4712</h3>
+          </div>
+          <div className={style.more_info}>
+            <a href="#">More Info</a>
+          </div>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Tours
+export default Tours;
